c2d5da29a89dd31b9bb41462f25357632a06031e
AcJoker <qianfeisj@yeah.net>
2016-07-25 10:40:50 +0800

    dust

--- iss.py
+++ iss.py
@@ -2,6 +2,7 @@
 import scrapy
 from bs4 import BeautifulSoup
 import json
+import win32api
 
 class IssSpider(scrapy.Spider):
     name = "iss"
@@ -24,7 +25,8 @@ class IssSpider(scrapy.Spider):
 			if i.string:
 				if i.string.find(':') > -1:
 					info.append(i.string[i.string.find(':') + 1:])
-        data = {"configs":[{"server":info[0],"server_port":int(info[1]),"password":int(info[2]),"method":info[3],"remarks":""},{"server":info[4],"server_port":int(info[5]),"password":int(info[6]),"method":info[7],"remarks":""},{"server":info[8],"server_port":int(info[9]),"password":int(info[10]),"method":info[11],"remarks":""}],"strategy" : None,"index" : 0,"global" : False,"enabled" : False,"shareOverLan" : False,"isDefault" : False,"localPort" : 1080,"pacUrl" : None,"useOnlinePac" : False}
+        data = {"configs":[{"server":info[0],"server_port":int(info[1]),"password":int(info[2]),"method":info[3],"remarks":""},{"server":info[4],"server_port":int(info[5]),"password":int(info[6]),"method":info[7],"remarks":""},{"server":info[8],"server_port":int(info[9]),"password":int(info[10]),"method":info[11],"remarks":""}],"strategy" : None,"index" : 0,"global" : False,"enabled" : True,"shareOverLan" : False,"isDefault" : False,"localPort" : 1080,"pacUrl" : None,"useOnlinePac" : False}
         with open('gui-config.json', 'w') as f:
         	f.write(json.dumps(data,sort_keys=False,indent=4).encode('utf-8'))
+        win32api.ShellExecute(0, 'open', 'Shadowsocks.exe', '','',1)
         pass
