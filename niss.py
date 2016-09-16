5b99f501a867d744ca5f799a81bde20e83703260
AcJoker <qianfeisj@yeah.net>
2016-09-16 10:58:03 +0800

    修正寻找当前目录的小飞机，可选择服务器

--- niss.py
+++ niss.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import re
-import os
-import urllib.request
-import json
-
+import re, urllib.request, json, os, sys
 _Author = "btjoker"
 
 url = 'http://www.ishadowsocks.org/'
@@ -19,10 +15,23 @@ result1 = re.findall(fwq, soup)
 result2 = re.findall(dk, soup)
 result3 = re.findall(pwd, soup)
 result4 = re.findall(jm, soup)
+while 1:
+	xz = input('请选择服务器：0.%s\t1.%s\t2.%s' % (result1[0], result1[1], result1[2]))
+	if xz.isalnum():
+		if xz in ['0', '1', '2']:
+			print('服务器：%s' % result1[int(xz)])
+			break
+		else:
+			print('请输入0-2的数值！')
+			continue
+	else:
+		print('请输入数字!')
+		continue
+data = {"configs":[{"server":result1[0],"server_port":int(result2[0]),"password":int(result3[0]),"method":result4[0],"remarks":""},{"server":result1[1],"server_port":int(result2[1]),"password":int(result3[1]),"method":result1[1],"remarks":""},{"server":result1[2],"server_port":int(result2[2]),"password":int(result3[2]),"method":result4[2],"remarks":""}],"strategy" : None,"index" : int(xz),"global" : False,"enabled" : True,"shareOverLan" : False,"isDefault" : False,"localPort" : 1080,"pacUrl" : None,"useOnlinePac" : False}
 
-data = {"configs":[{"server":result1[0],"server_port":int(result2[0]),"password":int(result3[0]),"method":result4[0],"remarks":""},{"server":result1[1],"server_port":int(result2[1]),"password":int(result3[1]),"method":result1[1],"remarks":""},{"server":result1[2],"server_port":int(result2[2]),"password":int(result3[2]),"method":result4[2],"remarks":""}],"strategy" : None,"index" : 0,"global" : False,"enabled" : True,"shareOverLan" : False,"isDefault" : False,"localPort" : 1080,"pacUrl" : None,"useOnlinePac" : False}
-
-with open('gui-config.json', 'wb') as f:
+with open(r'D:\pytemp\iss\gui-config.json', 'wb') as f:
     f.write(json.dumps(data,sort_keys=False,indent=4).encode('utf-8')) 
 
-os.popen('Shadowsocks.exe')
+soft = os.path.split(sys.argv[0])[0] +'\\Shadowsocks.exe'
+os.popen(soft)
+print('程序运行正常！')
