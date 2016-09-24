0fe0938221c7fb6563b114c1c3953c9ba9af3330
AcJoker <qianfeisj@yeah.net>
2016-09-24 23:23:34 +0800

    二次修正

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Spiral-submerged
 
-## niss.py 原生库版小飞机账号爬虫
+## niss.py 原生库版小飞机ss账号爬虫
 仅供测试(运行py文件时，寻找py文件同目录下的文件，抓取完毕自动运行小飞机，期间可以选择需要那个服务器）
+
 
 ## gs.py 归属地查询
 使用的是老马哥的api(使用方式为：python gs.py 13xxxxxxxxx 这样的方式，或者运行脚本在input处输入号码)
--- niss.py
+++ niss.py
@@ -15,25 +15,18 @@ result1 = re.findall(fwq, soup)
 result2 = re.findall(dk, soup)
 result3 = re.findall(pwd, soup)
 result4 = re.findall(jm, soup)
-while 1:
-	xz = input('请选择服务器：0.%s\t1.%s\t2.%s' % (result1[0], result1[1], result1[2]))
-	if xz.isalnum():
-		if xz in ['0', '1', '2']:
-			print('服务器：%s' % result1[int(xz)])
-			break
-		else:
-			print('请输入0-2的数值！')
-			continue
+for i in range(len(result3)):
+	if not result3[i]:
+		result3[i] = 0
 	else:
-		print('请输入数字!')
-		continue
+		xz = i
 data = {"configs":[{"server":result1[0],"server_port":int(result2[0]),"password":int(result3[0]),"method":result4[0],"remarks":""},{"server":result1[1],"server_port":int(result2[1]),"password":int(result3[1]),"method":result1[1],"remarks":""},{"server":result1[2],"server_port":int(result2[2]),"password":int(result3[2]),"method":result4[2],"remarks":""}],"strategy" : None,"index" : int(xz),"global" : False,"enabled" : True,"shareOverLan" : False,"isDefault" : False,"localPort" : 1080,"pacUrl" : None,"useOnlinePac" : False}
 
 with open(r'D:\pytemp\iss\gui-config.json', 'wb') as f:
     f.write(json.dumps(data,sort_keys=False,indent=4).encode('utf-8')) 
 
 soft = os.path.split(sys.argv[0])[0] +'\\Shadowsocks.exe'
-if sys.path.exists(soft):
+if os.path.exists(soft):
 	os.popen(soft)
 	print('程序运行正常！')
 else:
